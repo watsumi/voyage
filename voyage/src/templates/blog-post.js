4c3022bf75e81f38aa2e8e02bbe1f3488e860bbb
watsumi <w.utsumi2@gmail.com>
2023-05-02 19:17:42 +0900

    add gatsby-remark-katex - 数式を使えるようにした

--- voyage/src/templates/blog-post.js
+++ voyage/src/templates/blog-post.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
+import "katex/dist/katex.min.css"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
